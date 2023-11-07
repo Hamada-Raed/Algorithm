dc66a74ef18b6b1bb87a11a2906df055b2c03202
Hamada-Raed <stu.no1663@gmail.com>
2023-11-07 22:46:51 +0200

    Vowel count

--- Kata/Second-day.js
+++ Kata/Second-day.js
@@ -34,6 +34,26 @@ function sumPairs(ints, s) {
             haveSeen[currentNum] = true;
         }
     }
-    return undefined
+    return haveSeen
 }
 console.log(sumPairs([1, 2, 1, 2, 1], 3))
+
+
+//////////////////////////////////////// ////// 
+
+function getCount(str) {
+    var vowelsCount = 0;
+    var vowels = ["a", "e", "i", "o", "u"];
+    for (var i = 0; i < str.length; i++) {
+        for (var j = 0; j < vowels.length; j++) {
+            if (str[i] === vowels[j]) {
+                vowelsCount++;
+            }
+        }
+    }
+
+    return vowelsCount;
+} 
+
+/////////////////////////////////// 
+
