f11edf6c054b0f8c0a952f715ea4f6b8cc34b73d
Hamada-Raed <stu.no1663@gmail.com>
2023-11-10 19:46:19 +0200

    smiley faces

--- Kata/third-day.js
+++ Kata/third-day.js
@@ -14,7 +14,39 @@ function filter_list(l) {
 }
 
 function filter_list(l) {
-    return l.filter((currentItem)=> {
+    return l.filter((currentItem) => {
         return typeof currentItem === 'number'
     })
+}
+
+///////////////////////////// 
+//return the total number of smiling faces in the array 
+
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
+function countSmileys(arr) {
+    // a place to keep track of the current count
+    let count = 0
+    // iterate over the arr
+    for (var i = 0; i < arr.length; i++) {
+        // iterate over the current face if the length of the face is either 2 or 3
+        const face = arr[i]
+        if (face.length == 2 || face.length == 3) {
+            // if the first character is a : or ; 
+            if (face[0] == ':' || face[0] == ';') {
+                // if the second character is a - or ~
+                if (face[1] == '-' || face[1] == '~') {
+                    // if the third character is a ) or D  
+                    if (face[2] == 'D' || face[2] == ')') {
+                        //increment the count
+                        count++
+                    }
+                }
+                else if ((face[1] == 'D' || face[1] == ')') && face.length == 2) {
+                    count++
+                }
+            }
+        }
+    }
+    return count;
 }
