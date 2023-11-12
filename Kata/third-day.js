87b29bf5d14c391b54155d005609810391b985c4
Hamada-Raed <stu.no1663@gmail.com>
2023-11-12 07:57:38 +0200

    last commit in the third day

--- Kata/third-day.js
+++ Kata/third-day.js
@@ -156,6 +156,7 @@ console.log(maxSequence([1, 2, 3, 4, 5]))
 
 
 ////////////////////////////////////// 
+// Kadane's Algorithm (**)
 
 var maxSequence = function (arr) {
     // place keep track of currentSum // initialize to 0
@@ -174,4 +175,57 @@ var maxSequence = function (arr) {
     }
 
     return maxSum;
+}
+ 
+//////////////////////////
+// Sum string as number 
+
+function sumStrings(a, b) {
+    let firstNumber = Number(a)
+    let secondNumber = Number(b)
+    let sum = firstNumber + secondNumber
+    return sum.toString()
+    // return (Number(a) + Number(b)).toString(); this only works for smallest numbers 
+
+} 
+
+///////////////////////////// 
+
+// Sum string aS number (**)
+
+function sumStrings(a, b) {
+    //   return (Number(a) + Number(b)).toString(); this only works for smaller numbers
+    if (a.length < b.length) {
+        while (a.length != b.length) {
+            a = '0' + a;
+        }
+    } else if (b.length < a.length) {
+        while (a.length != b.length) {
+            b = '0' + b;
+        }
+    }
+
+    let sum = '';
+    let carry = 0;
+    for (let i = a.length - 1; i >= 0; i--) {
+        const aNum = +a[i];
+        const bNum = +b[i];
+        const innerSum = aNum + bNum + carry;
+
+        let placeNum = innerSum;
+        if (innerSum > 9) {
+            placeNum = innerSum % 10;
+            carry = Math.floor(innerSum / 10);
+        } else {
+            carry = '';
+        }
+        sum = placeNum + sum;
+    }
+    sum = carry + sum;
+
+    while (sum[0] == '0') {
+        sum = sum.slice(1);
+    }
+
+    return sum;
 }
