b1b34b5b72e0eb293e4290481ce280bdc73e6b0b
Hamada-Raed <stu.no1663@gmail.com>
2023-11-15 20:09:13 +0200

    Multiples of 3 or 5

--- Kata/5th.js
+++ Kata/5th.js
@@ -22,4 +22,30 @@ function isIsogram(str) {
     }
     return true
 }
-console.log(isIsogram('hHEelo'))
+console.log(isIsogram('hHEelo')) 
+
+///////////////////////////// 
+// Multiples of 3 or 5
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+// we get 3, 5, 6 and 9. The sum of these multiples is 23
+function solution(number) {
+    // a place to store the number that divide 3 or 5 under the provided number (divides)
+    let divides = [];
+    // a place to store the sum
+    let sum = 0;
+    // iterate up the number
+    for (var i = 1; i < number; i++) {
+        // if the number is divided of 3 or 5 
+        if (i % 3 == 0 || i % 5 == 0) {
+            // push it divdies 
+            divides.push(i)
+        }
+    }
+    console.log(divides)
+    // iterate again and and find the sum
+    for (var j = 0; j < divides.length; j++) {
+        sum += divides[j]
+    }
+    return sum
+}
+console.log(solution(10))
