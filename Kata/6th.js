5fbf6ccd628b2ce6f250e430dea2a5d19218a990
Hamada-Raed <stu.no1663@gmail.com>
2023-11-18 22:58:16 +0200

    new one

--- Kata/6th.js
+++ Kata/6th.js
@@ -125,3 +125,45 @@ function openOrSenior(data) {
 function openOrSenior(data) {
     return data.map(([age, handicap]) => age >= 55 && handicap > 7 ? 'Senior' : 'Open');
 }
+
+////////////////////////////////////////////////// 
+
+
+const MAX_PRIOR_SPELLS = 13;
+
+class Wand {
+    constructor(spells = {}) {
+        this.casted = [];
+        Object.assign(this, spells);
+
+        return new Proxy(this, {
+            get: (target, property) => {
+                const value = target[property];
+                if (typeof value === 'function') {
+                    target.casted.unshift(property);
+                }
+                return value;
+            }
+        });
+    }
+
+    prioriIncantatem() {
+        return this.casted.slice(1, MAX_PRIOR_SPELLS + 1);
+    }
+
+    deletrius() {
+        this.casted = ['deletrius'];
+    }
+}
+
+const w = new Wand({
+    expelliarmus: function () { },
+    alohomora: function () { },
+    avadaKedavra: function () { }
+});
+
+w.alohomora();
+w.expelliarmus();
+w.avadaKedavra();
+
+console.log(w.prioriIncantatem(), ['avadaKedavra', 'expelliarmus', 'alohomora']);g
