fc4c011dd5e72aba6acfaedba0009d2a8281a676
Hamada-Raed <stu.no1663@gmail.com>
2023-11-19 21:46:32 +0200

    new one

--- Kata/7th.js
+++ Kata/7th.js
@@ -7,3 +7,26 @@
 function anagrams(word, words) {
 
 }
+
+////////////////////////////// 
+
+function songDecoder(song) {
+    // split the string on WUB
+    const split = song.split('WUB');
+    console.log(split);
+
+    // filter out empty strings
+    const words = split.filter(word => word.trim());
+    console.log(words);
+
+    // join back together on a space
+    const result = words.join(' ');
+
+    return result;
+}
+
+function songDecoder(song) {
+    return song.replace(/(WUB)+/g, ' ').trim();
+}
+
+console.log(songDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB"));  
