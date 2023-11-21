5ea8733c6725efb3810e0f3b34979ddf1ab7b931
Hamada-Raed <stu.no1663@gmail.com>
2023-11-21 04:47:03 +0200

    RGB

--- Kata/8th.js
+++ Kata/8th.js
@@ -20,6 +20,11 @@ function anagrams(word, words) {
 // 0, 0, 0       -- > "000000"
 // 148, 0, 211   -- > "9400D3"
 
+const withinRange = value => value < 0 ? 0 : value > 255 ?  255 : value; 
+const toHex = value => pad(value.toString(16).toUpperCase());
+const pad = value => value.length === 1 ? "0" + value : value; 
+const convert = value => toHex(withinRange(value))
+
 function rgb(r, g, b) {
-    return "FFFFFF";
+    return `${convert(r)}${convert(g)}${convert(b)}`
 }
