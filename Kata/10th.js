dc3f59cf52469342adef9e326b1b0fa3ec436148
Hamada-Raed <stu.no1663@gmail.com>
2023-11-28 03:24:52 +0200

    Rot13

--- Kata/10th.js
+++ Kata/10th.js
@@ -125,7 +125,27 @@ function solution(a, b) {
     }
     return result
 }
-console.log(solution('45', '1')) 
+console.log(solution('45', '1'))  
+
+////////////////////////////////////// 
+
+// Rot13  
+
+function rot13(str) {
+    return str.replace(/[a-zA-Z]/g, function (char) {
+        var charCode = char.charCodeAt(0);
+        var offset = charCode <= 90 ? 65 : 97;
+        return String.fromCharCode((charCode - offset + 13) % 26 + offset);
+    });
+}
+
+// Example usage:
+var originalString = "Hello, World!";
+var encryptedString = rot13(originalString);
+
+console.log("Original: " + originalString);
+console.log("Encrypted: " + encryptedString);
+
 
 
 
