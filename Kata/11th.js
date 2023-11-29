382aec564cfa5e8b5f9d0a4af2e6369550be3b01
Hamada-Raed <stu.no1663@gmail.com>
2023-11-29 16:42:32 +0200

    Avg

--- Kata/11th.js
+++ Kata/11th.js
@@ -3,3 +3,14 @@ function evenOrOdd(number) {
     return number % 2 == 0 ? 'Even' : 'Odd';
 } 
 
+// Avarage  
+
+function findAverage(array) {
+    let sum = 0;
+    if (array.length == 0) return 0;
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i]
+    }
+
+    return sum / array.length;
+}
