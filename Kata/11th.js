0318b9328183ea79083a1e4978bb2ad1bb471d04
Hamada-Raed <stu.no1663@gmail.com>
2023-11-30 05:54:31 +0200

    new correct the string mistake

--- Kata/11th.js
+++ Kata/11th.js
@@ -18,4 +18,16 @@ function findAverage(array) {
 // split string 
 function stringToArray(string) {
     return string.split(' ')
+} 
+
+//Correct the mistakes of the character recognition software 
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+
+function correct(string) {
+    string = string.replace(/5/g, 'S') // g : globle 
+    string = string.replace(/0/g, 'O')
+    string = string.replace(/1/g, 'I')
+    return string
 }
