2aac1dfe28443d4eb6f78a1b53f6f5b2299f270a
Hamada-Raed <stu.no1663@gmail.com>
2023-12-07 21:26:31 +0200

    new commit

--- Kata/12th.js
+++ Kata/12th.js
@@ -121,4 +121,15 @@ function tArea(tStr) {
     return (side * side) / 2
 } 
 
+// Plus - minus - plus - plus - ... - Sum
+
+function getSolution(arr, sum) {
+    let sumArr = 0;
+    for (var i=0; i<arr.length; i++){
+        if (arr[i] < sum) {
+            
+        } 
+    }
+}
+console.log(getSolution([1, 3, 4, 6, 8], 20))
 
