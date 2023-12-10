819e8c1e21cec1c7e670f322af70ec8f1cbb8bcd
Hamada-Raed <stu.no1663@gmail.com>
2023-12-10 17:05:28 +0200

    new commit

---
+++ Kata/13th.js
@@ -0,0 +1,13 @@
+// Nesting Structure Comparison 
+// Complete the function/method (depending on the language) to 
+// return true/True when its argument is an array that has the same 
+// nesting structures and same corresponding
+//  length of nested arrays as the first array. 
+
+Array.prototype.sameStructureAs = function (other) {
+    // Return 'true' if and only if 'other' has the same
+    // nesting structure as 'this'.
+
+    // Note: You are given a function isArray(o) that returns
+    // whether its argument is an array.
+};
