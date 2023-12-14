f89a36a825dbc6ed10666b80055f66e0d2c03f4a
Hamada-Raed <stu.no1663@gmail.com>
2023-12-14 21:20:58 +0200

    Find the divisors

--- Kata/13th.js
+++ Kata/13th.js
@@ -6,4 +6,21 @@ function oneDown(str) {
 
 
 }
-oneDown('')
+oneDown('')  
+
+// Find the divisors 
+
+function divisors(integer) {
+    let divisorsArr = []
+    for (var i = 2; i < integer; i++) {
+        if (integer % i == 0) {
+            divisorsArr.push(i);
+        }
+    }
+    if (divisorsArr.length == 0) {
+        return integer + ' is prime'
+    }
+    return divisorsArr
+};
+
+// 
