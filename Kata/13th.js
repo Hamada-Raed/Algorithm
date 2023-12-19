139653f72e0b32fc769cc49cc4c77f363875b949
Hamada-Raed <stu.no1663@gmail.com>
2023-12-19 18:28:35 +0200

    new commit

--- Kata/13th.js
+++ Kata/13th.js
@@ -49,3 +49,21 @@ function spinWords(words) {
         return (word.length > 4) ? word.split('').reverse().join('') : word;
     }).join(' ');
 }
+
+// array methds 
+
+function indexOf(array, value) {
+    for (let i = 0; i < array.length; i += 1) {
+        if (array[i] === value) return i;
+    }
+    return -1;
+}
+
+console.log(indexOf([42], 42), 0);
+const people = ['CJ', 'CyberCyclonic', 'strategefirst'];
+console.log(indexOf(people, 42));
+console.log(indexOf(people, 'CJ'));
+console.log(indexOf(people, 'John'));
+console.log(indexOf(people, 'strategefirst'));
+
+console.log(people.indexOf('CJ'));
