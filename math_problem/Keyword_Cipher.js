a3f5ae39e9836219bf1ba4ac738ea49423a63c83
Hamada-Raede <hamadaraed19@gmail.com>
2025-07-05 21:42:04 +0300

    problems on cipher

---
+++ math_problem/Keyword_Cipher.js
@@ -0,0 +1,18 @@
+
+//               abcdefghijklmnopqrstuvwxyz
+//   hello ==>   |||||||||||||||||||||||||| ==> bshhk
+//               wednsaybcfghijklmopqrtuvxz
+
+function keywordCipher(string, keyword){
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"; 
+
+    
+
+    console.log(cipher);
+
+
+    
+
+}
+
+console.log(keywordCipher("hello","wednesday"), "bshhk");
